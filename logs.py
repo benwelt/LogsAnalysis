8a5cc41020d603265804ac0cb3b653bb82f626b8
Ben Haas <haas.ben4@gmail.com>
2017-09-12 22:00:22 -0600

    Complete two queries

--- logs.py
+++ logs.py
@@ -10,13 +10,18 @@ questions = [
             "Who are the most popular authors of all time?",
             "On which days did more than 1% of requests lead to errors?"
             ]
-q1 = ""
+q1 = """SELECT articles.title,COUNT(*) FROM articles,log WHERE log.path like
+    concat('%' ,articles.slug) group by articles.title
+    order by count desc limit 3"""
 
 # Who are the most popular authors of all time?
-q2 = ""
+q2 = """SELECT authors.name,COUNT(*) FROM articles,authors,log WHERE
+    articles.author = authors.id AND log.path like concat('%' ,articles.slug)
+    group by authors.name order by count desc"""
 
 # On which days did more than 1% of requests lead to errors?
-q3 = ""
+#q3 = """SELECT DATE(time),COUNT(*) FROM log WHERE
+#    status LIKE '4%' OR status LIKE '5%' group by DATE(time)"""
 
 # Holds the result of the query
 query_result = {}

6e13459c8c4f372a447cccb32a4d994ce1486808
Ben Haas <haas.ben4@gmail.com>
2017-09-17 22:11:44 -0600

    Add 3rd query

--- logs.py
+++ logs.py
@@ -10,18 +10,18 @@ questions = [
             "Who are the most popular authors of all time?",
             "On which days did more than 1% of requests lead to errors?"
             ]
+
 q1 = """SELECT articles.title,COUNT(*) FROM articles,log WHERE log.path like
     concat('%' ,articles.slug) group by articles.title
     order by count desc limit 3"""
 
-# Who are the most popular authors of all time?
 q2 = """SELECT authors.name,COUNT(*) FROM articles,authors,log WHERE
     articles.author = authors.id AND log.path like concat('%' ,articles.slug)
     group by authors.name order by count desc"""
 
-# On which days did more than 1% of requests lead to errors?
-#q3 = """SELECT DATE(time),COUNT(*) FROM log WHERE
-#    status LIKE '4%' OR status LIKE '5%' group by DATE(time)"""
+q3 = """SELECT DATE(time), ROUND(100.00 * SUM(CASE status WHEN '200 OK'
+    THEN 0 ELSE 1 END)/COUNT(status),2) AS Percent GROUP BY DATE(time)
+    ORDER BY Percent DESC"""
 
 # Holds the result of the query
 query_result = {}
@@ -30,6 +30,6 @@ query_result = {}
 def query_log(query):
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    result = c.execute(query)
-    return result
+    c.execute(query)
+    return c.fetchall()
     db.close()
